498d822b150a8143bc52ab468dfd37e04ec62b64
Pharia Le <pharia.le@gmail.com>
2020-03-21 03:35:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ require_relative './course.rb'
 
 class Scraper
   
-def get_page
+  def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
  
     # doc.css(".post").each do |post|
@@ -19,15 +19,15 @@ def get_page
   
   def get_courses
     self.get_page.css(".post")
-end
-
-def make_courses
-  self.get_courses.each do |post|
-    course = Course.new
-    course.title = post.css("h2").text
-    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
   end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
 end
   
   def print_courses
