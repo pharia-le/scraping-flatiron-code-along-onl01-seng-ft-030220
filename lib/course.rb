dec429e0c6f2df5be3c1aa81919d0353acb9f45f
Pharia Le <pharia.le@gmail.com>
2020-03-21 03:17:20 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,5 +2,17 @@ class Course
   
   attr_accessor :title, :schedule, :description
   
+  def initialize
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
 end
 
